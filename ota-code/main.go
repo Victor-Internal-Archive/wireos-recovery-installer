080060cddbd210937dac256bac1512ae6a317f04
Switch-modder <deolrajjyot@gmail.com>
2026-09-11 16:19:33 -0400

    Remove delay between aboot and recovery writing

--- ota-code/main.go
+++ ota-code/main.go
@@ -268,7 +268,6 @@ func main() {
 	updateTotalPercentage(0)
 
 	dump("Writing ABOOT", "/anki/ankidev-signed.img.gz", "/dev/block/bootdevice/by-name/aboot", 0, 3)
-	time.Sleep(time.Millisecond * 1000)
 	dump("Writing Recovery", "/anki/recovery.img.gz", "/dev/block/bootdevice/by-name/recovery", 4, 8)
 	dump("Writing RecoveryFS", "/anki/recoveryfs.img.gz", "/dev/block/bootdevice/by-name/recoveryfs", 9, 100)
 
