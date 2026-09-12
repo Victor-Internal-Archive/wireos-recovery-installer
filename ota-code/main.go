17c75f6139d72c4a3ea4bbfd2c4a4e7953f5aef6
Switch-modder <deolrajjyot@gmail.com>
2026-09-12 15:36:38 -0400

    Change total to upgrade

--- ota-code/main.go
+++ ota-code/main.go
@@ -138,7 +138,7 @@ func displayRenderer() {
 			Color: color.RGBA{R: 255, G: 255, B: 255, A: 255},
 		})
 		lines = append(lines, vscreen.Line{
-			Text:  "Total progress:",
+			Text:  "Upgrade progress:",
 			Color: color.RGBA{R: 255, G: 255, B: 255, A: 255},
 		})
 		lines = append(lines, vscreen.Line{
